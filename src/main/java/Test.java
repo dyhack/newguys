288020d987a56a2617df1decdc70d4e1444165c0
刘兴博 <2453801419.@qq.com>
2018-10-20 16:52:18 +0800

    我加了一条输出

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,12 @@
 
 public class Test {
 
-	public static void main(String[] args) {
-		{System.out.println("Hello World");}
+	public static void main(String[] args)
+	{
+		{
+			System.out.println("Hello World");
+			System.out.println("lllllllll");
+		}
 		
 
 	}
